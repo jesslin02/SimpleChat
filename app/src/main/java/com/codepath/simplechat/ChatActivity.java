4ddf19f7699bdce6870a5af12a797b53b38cb375
jessica lin <53272628+jesslin02@users.noreply.github.com>
2021-07-06 09:02:24 -0700

    bind adapter to recycler view

--- app/src/main/java/com/codepath/simplechat/ChatActivity.java
+++ app/src/main/java/com/codepath/simplechat/ChatActivity.java
@@ -1,6 +1,8 @@
 package com.codepath.simplechat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -10,21 +12,30 @@ import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.codepath.simplechat.models.Message;
+import com.parse.FindCallback;
 import com.parse.LogInCallback;
 import com.parse.ParseAnonymousUtils;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChatActivity extends AppCompatActivity {
     static final String TAG = ChatActivity.class.getSimpleName();
-
+    static final int MAX_CHAT_MESSAGES_TO_SHOW = 50;
     static final String USER_ID_KEY = "userId";
     static final String BODY_KEY = "body";
 
     EditText etMessage;
     ImageButton ibSend;
+    RecyclerView rvChat;
+    List<Message> mMessages;
+    boolean mFirstLoad;
+    ChatAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +73,17 @@ public class ChatActivity extends AppCompatActivity {
         // Find the text field and button
         etMessage = (EditText) findViewById(R.id.etMessage);
         ibSend = (ImageButton) findViewById(R.id.ibSend);
+        rvChat = (RecyclerView) findViewById(R.id.rvChat);
+        mMessages = new ArrayList<>();
+        mFirstLoad = true;
+        final String userId = ParseUser.getCurrentUser().getObjectId();
+        mAdapter = new ChatAdapter(ChatActivity.this, userId, mMessages);
+        rvChat.setAdapter(mAdapter);
+
+        // associate the LayoutManager with the RecylcerView
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(ChatActivity.this);
+        linearLayoutManager.setReverseLayout(true);
+        rvChat.setLayoutManager(linearLayoutManager);
 
         // When send button is clicked, create message object on Parse
         ibSend.setOnClickListener(new View.OnClickListener() {
@@ -93,5 +115,33 @@ public class ChatActivity extends AppCompatActivity {
         });
     }
 
+    // Query messages from Parse so we can load them into the chat adapter
+    void refreshMessages() {
+        // Construct query to execute
+        ParseQuery<Message> query = ParseQuery.getQuery(Message.class);
+        // Configure limit and sort order
+        query.setLimit(MAX_CHAT_MESSAGES_TO_SHOW);
+
+        // get the latest 50 messages, order will show up newest to oldest of this group
+        query.orderByDescending("createdAt");
+        // Execute query to fetch all messages from Parse asynchronously
+        // This is equivalent to a SELECT query with SQL
+        query.findInBackground(new FindCallback<Message>() {
+            public void done(List<Message> messages, ParseException e) {
+                if (e == null) {
+                    mMessages.clear();
+                    mMessages.addAll(messages);
+                    mAdapter.notifyDataSetChanged(); // update adapter
+                    // Scroll to the bottom of the list on initial load
+                    if (mFirstLoad) {
+                        rvChat.scrollToPosition(0);
+                        mFirstLoad = false;
+                    }
+                } else {
+                    Log.e("message", "Error Loading Messages" + e);
+                }
+            }
+        });
+    }
 
 }
